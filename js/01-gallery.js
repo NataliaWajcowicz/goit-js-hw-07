a131f6186628fb30dbb6882c19bdee45039c2668
NataliaWajcowicz <n.wajcowicz@o2.pl>
2022-11-14 13:06:09 +0100

    create gallery 01

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -2,3 +2,18 @@ import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
 console.log(galleryItems);
+
+const gallery = document.querySelector(".gallery");
+console.log(gallery);
+
+createGalleryItems();
+
+function createGalleryItems() {
+   for( const item of galleryItems) {
+       const galleryElements = gallery.insertAdjacentHTML("beforeEnd", `<div class='gallery__item'><a class='gallery__link' href='${item.original}'><img class='gallery__image' src='${item.preview}' data-source='${item.original}' alt='${item.description}'>`);
+       
+        
+       
+        
+    }
+}
